20bd772060c05d20ecf6a2dc8246bbe8136bccc6
MattHelton <mjoelhelton@me.com>
2019-05-27 19:15:35 -0400

    Creates yarn build

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,7 @@ import './home.css';
 
 const Home = () => (
   <div id="home">
-    <h1>Welcome to React Dice!</h1>
+    <h1>Welcome to React Dice</h1>
     <h2>Choose a game</h2>
     <div id="choose-cee-lo">
       <a href="/ceelo">Cee Lo</a>

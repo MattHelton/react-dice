c60d05d224fb2288d0e163de96cf6f288d04cdbb
MattHelton <mjoelhelton@me.com>
2019-05-27 17:01:18 -0400

    Adds styling to score component, moving score to bottom of the screen

--- src/components/score.jsx
+++ src/components/score.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
+import './score.css';
 
 const Score = ({ currentScore }) => {
   if (!currentScore) {
     return <div />;
   }
   return (
-    <React.Fragment>
+    <div id="score">
     Your current score is :
       {' '}
       {currentScore}
-    </React.Fragment>
+    </div>
   );
 };
 
